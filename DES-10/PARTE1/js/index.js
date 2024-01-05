2fb7d979a468f2cd56a551d791ee3c6755358e6f
briancalde92 <137201879+briancalde92@users.noreply.github.com>
2024-01-05 00:24:46 -0300

    Add files via upload

---
+++ DES-10/PARTE1/js/index.js
@@ -0,0 +1,62 @@
+// Ejercicio 2:
+class Libro {
+    constructor (titulo, autor) {
+        this.autor = autor;
+        this.titulo = titulo;
+    }
+    
+    // Ejercicio 3:
+    get oracion() {
+        return `${this.titulo}, de ${this.autor.toUpperCase()}`;
+    }
+    mostrarDatosEnConsola() {
+        console.log(this.oracion)
+    }
+    // Ejercicio 4:
+    mostrarDatosEnAlert() {
+        alert(this.oracion)
+    }
+    // Ejercicio 5:
+    getTitulo() {
+        return this.titulo;
+    }
+    // Ejercicio 6:
+    mostrarDatos() {
+        return this.oracion;
+    }
+}
+
+// Ejercicio 2:
+let unLibro = new Libro('Ángeles y Demonios', 'Dan Brown');
+
+// Ejercicio 5:
+let nuevolibro = new Libro('Fuego y Sangre', 'George R. R. Martin');
+
+let tituloDelNuevoLibro = nuevolibro.getTitulo();
+alert(tituloDelNuevoLibro);
+
+// Ejercicio 6:
+let tituloDelNuevoLibro1 = nuevolibro.mostrarDatos();
+
+function mostrarDatos(modo) {
+if (modo === 'alert') {
+        // return this.mostrarDatosEnAlert;
+        alert(tituloDelNuevoLibro1);  
+    } else if (modo === 'console'){
+        // return this.mostrarDatosEnConsola;
+        return console.log(tituloDelNuevoLibro1);
+    }  
+    else 
+    // return this.mostrarDatosEnConsola;
+return console.log(tituloDelNuevoLibro1);
+}
+mostrarDatos('alert');
+// mostrarDatos('console');
+// mostrarDatos() ;
+
+unLibro.mostrarDatosEnConsola();
+unLibro.mostrarDatosEnAlert();
+unLibro.getTitulo();
+unLibro.mostrarDatos();
+
+console.log(unLibro);
